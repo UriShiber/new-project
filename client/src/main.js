ea009380b71ce88d174f68e5b8ff2c8a8867b327
UriShiber <urishiber@gmail.com>
2020-04-11 19:41:18 +0300

    Feat(Client): Add vuetify and bulma

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "@/plugins/vuetify";
+import bulma from "bulma/css/bulma.css";
 import router from "./router";
 import store from "./store";
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  bulma,
   render: h => h(App)
 }).$mount("#app");
